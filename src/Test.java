507a63c195db898365f3c0fb4e880e193cd524b0
Class5-Pc10 <hilltohill@rambler.ru>
2019-10-03 20:43:08 +0300

    коммит 1

--- src/Test.java
+++ src/Test.java
@@ -2,13 +2,5 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Test");
-        System.out.println("Test2");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
     }
 }
