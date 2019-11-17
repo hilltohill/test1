565115e89bbbd9f5c28ceda094c720dcd1bd9120
Class5-Pc10 <hilltohill@rambler.ru>
2019-11-17 10:07:42 +0200

    new int

--- src/Test.java
+++ src/Test.java
@@ -5,8 +5,5 @@ public class Test {
         System.out.println("Test2");
         System.out.println("Test");
         System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
     }
 }
